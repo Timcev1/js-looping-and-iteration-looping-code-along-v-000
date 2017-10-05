15f7a63c4ad71e66308ab2dc73bc2e00c96ba65c
Timothy Cevallos <timcev@gmail.com>
2017-10-05 22:23:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function printBadges(array){
   }
   return array
 }
+
+function tailsNeverFails(){
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
